a5274a014ccdb05b5c9d8e948fccb50048b6b9b2
Dayse <daysemaranhao@gmail.com>
2023-02-15 14:41:09 -0300

    Delete prediction.py

--- prediction.py
+++
@@ -1,9 +0,0 @@
-def Prediction(model,x_test):
-
-    # predict
-    predictions = model.predict(x_test)
-    predictions = predictions > 0.5 # for binary
-
-    print(predictions)
-    
-    #return predictions
